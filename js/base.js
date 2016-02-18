602186f491419ad63a51da2ba1b1a26ccef4ac6d
teles <josetelesmaciel@gmail.com>
2016-02-18 21:20:59 -0200

    Adicionando o AjaxModel no rolê

--- js/base.js
+++ js/base.js
@@ -3,8 +3,29 @@ if(!window.GUEST){
 	var GUEST = {};
 }
 
+if(!GUEST.angularDependencies){
+	GUEST.angularDependencies = [];
+}
+
+
 (function(){
+	'use strict';
+
 	var angularDependencies = [];
-	angular.module('guest-post', angularDependencies);	
+	
+	angular.module('guest-post', GUEST.angularDependencies);	
+
+	angular.module("guest-post").config(guestConfig);
+	angular.module("guest-post").run(guestRun);
+
+	function guestConfig($interpolateProvider, $httpProvider, $locationProvider){
+	    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+	    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';  
+		delete $httpProvider.defaults.headers.common['X-Requested-With'];
+		$locationProvider.html5Mode(true);		
+	}
+
+	function guestRun(){}
+
 })();
 

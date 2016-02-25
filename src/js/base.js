3ff45330fb91a70c10f68043ff6c6f0295ea691e
Jota Teles <jose.maciel@vivadecora.com.br>
2016-02-25 10:42:52 -0300

    closes #13 - adicionando editorconfig

--- src/js/base.js
+++ src/js/base.js
@@ -1,30 +1,28 @@
-
 if(!window.GUEST){
-	var GUEST = {};
+    var GUEST = {};
 }
 
 if(!GUEST.angularDependencies){
-	GUEST.angularDependencies = [];
+    GUEST.angularDependencies = [];
 }
 
 (function(){
-	'use strict';
-	
-	angular.module('guest-post', GUEST.angularDependencies);	
+    'use strict';
 
-	angular.module("guest-post").config(guestConfig);
-	angular.module("guest-post").run(guestRun);
+    angular.module('guest-post', GUEST.angularDependencies);
 
-	function guestConfig($interpolateProvider, $httpProvider, $locationProvider, SheetModelProvider, $urlRouterProvider){
-	    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-	    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';  
-		delete $httpProvider.defaults.headers.common['X-Requested-With'];
-		$locationProvider.html5Mode(true);	
-		SheetModelProvider.setSheetsuUrl('https://sheetsu.com/apis/v1.0/658fb938');
-		$urlRouterProvider.otherwise('/');
-	}
+    angular.module("guest-post").config(guestConfig);
+    angular.module("guest-post").run(guestRun);
 
-	function guestRun(){}
+    function guestConfig($interpolateProvider, $httpProvider, $locationProvider, SheetModelProvider, $urlRouterProvider){
+        $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        $locationProvider.html5Mode(true);
+        SheetModelProvider.setSheetsuUrl('https://sheetsu.com/apis/v1.0/658fb938');
+        $urlRouterProvider.otherwise('/');
+    }
+    function guestRun(){}
 
 })();
 
--- tasks/watch.js
+++ tasks/watch.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 
 module.exports = function() {
-	gulp.watch(['src/js/**/*.js', 'src/js/**/*.html'], ['copy', 'inject']);
+    gulp.watch(['src/js/**/*.js', 'src/js/**/*.html'], ['copy', 'inject']);
 };

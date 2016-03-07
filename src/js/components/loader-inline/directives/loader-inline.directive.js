d61cb60bebc0f42797f79d14a44d134c98a7f04c
teles <josetelesmaciel@gmail.com>
2016-03-07 00:09:35 -0300

    closes #26 - cria componente loader-inline

---
+++ src/js/components/loader-inline/directives/loader-inline.directive.js
@@ -0,0 +1,16 @@
+(function(){
+    'use strict';
+
+    angular.module('guest-post.loader-inline').directive('loaderInline', loaderInlineDirective);
+
+    function loaderInlineDirective(){
+        var directive = {
+            restrict: 'E',
+            templateUrl: '/js/components/loader-inline/templates/loader-inline.template.html',
+            scope: {},
+            replace: true
+        };
+        return directive;
+    }
+
+})();
